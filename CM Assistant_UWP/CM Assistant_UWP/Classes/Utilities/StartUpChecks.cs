ec45cc313caaba401ccfe9c7e0addb194da7af8b
Sami Richards <samirichards100@gmail.com>
2019-01-31 23:43:56 +0000

    Started implementing a setup page

---
+++ CM Assistant_UWP/CM Assistant_UWP/Classes/Utilities/StartUpChecks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CM_Assistant_UWP.Classes.Utilities
+{
+    class StartUpChecks
+    {
+        public bool IsFirstboot()
+        {
+            if (ApplicationData.Current.LocalSettings.Values["Loaded"] == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void SetFirstBoot()
+        {
+            ApplicationData.Current.LocalSettings.Values["Loaded"] = true;
+        }
+
+        public void ResetFirstBoot()
+        {
+            ApplicationData.Current.LocalSettings.Values["Loaded"] = null;
+        }
+    }
+}
