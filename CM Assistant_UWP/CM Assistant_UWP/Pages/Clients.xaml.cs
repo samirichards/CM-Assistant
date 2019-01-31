2429ead82851e587ac1072fc1a0ea6de0cb47ebe
Sami Richards <samirichards100@gmail.com>
2019-01-31 11:04:41 +0000

    Tried to make navigationview menu dynamic

--- CM Assistant_UWP/CM Assistant_UWP/Pages/Clients.xaml.cs
+++ CM Assistant_UWP/CM Assistant_UWP/Pages/Clients.xaml.cs
@@ -9,6 +9,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -22,9 +23,44 @@ namespace CM_Assistant_UWP.Pages
     /// </summary>
     public sealed partial class Clients : Page
     {
+        public List<NavigationViewItem> ClientList = new List<NavigationViewItem>();
+
         public Clients()
         {
             this.InitializeComponent();
+            NavigationViewItem temp = new NavigationViewItem
+            {
+                Content = "Test",
+                Icon = new SymbolIcon(Symbol.Account)
+            };
+            ClientList.Add(temp);
+            NavView_ClientsList.DataContext = ClientList;
+            NavView_ClientsList.MenuItemsSource = ClientList;
+        }
+
+        private void NavView_ClientsList_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+
+        }
+
+        private void NavView_ClientsList_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+
+        private void Btn_AddClient_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            NavigationViewItem temp = new NavigationViewItem
+            {
+                Content = "Test",
+                Icon = new SymbolIcon(Symbol.Account)
+            };
+            NavView_ClientsList.MenuItemsSource = ClientList;
+            ContentDialog content = new ContentDialog
+            {
+                Content = "Test",
+                CloseButtonText = "Alright then famalam"
+            };
         }
     }
 }
